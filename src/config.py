c49d1b50621731770b38c6e090ce4c99a7ab9139
maxbeier <you@example.com>
2021-05-14 10:03:30 +0200

    Init repo

---
+++ src/config.py
@@ -0,0 +1,4 @@
+import pathlib
+
+root_p = pathlib.Path(__file__).parent.parent
+data_p = pathlib.Path(root_p, "data")
---
+++ src/main.py
@@ -0,0 +1,9 @@
+import pathlib
+import pandas as pd
+import numpy as np
+
+
+class DataLoader(object):
+
+    def __init__():
+        pass
